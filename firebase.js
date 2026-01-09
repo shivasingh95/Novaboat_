3e159c3eac6b9288a57987b83d764eae00879f13
shivasingh95 <thakurshivasinghraghuwanshi@gmail.com>
2026-01-10 02:06:31 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
-import admin from "firebase-admin";
-import dotenv from "dotenv";
+const { initializeApp } = require("firebase/app");
 
-dotenv.config();
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-});
+initializeApp(firebaseConfig);
 
-// create Firestore instance
-const db = admin.firestore();
-
-export { db };
+console.log("🔥 Firebase initialized");
